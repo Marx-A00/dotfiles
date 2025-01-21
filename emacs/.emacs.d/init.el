1c54d246ef196ee2d0ccd0211be14b34ea545fbe
Marcos Andrade <mnandrade1999@gmail.com>
2025-01-21 11:07:55 -0600

    sending custom files to shadow realm + new theme

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -54,6 +54,7 @@
 (use-package no-littering
   :ensure t
   :config
+  (setq custom-file (no-littering-expand-etc-file-name "custom.el"))
   (no-littering-theme-backups))
 
 ;; Still need to fix #file showing up maybe
@@ -141,19 +142,3 @@
   :after (evil ivy)
   :config
   (evil-collection-init))
-
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("0d2c5679b6d087686dcfd4d7e57ed8e8aedcccc7f1a478cd69704c02e4ee36fe" default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
