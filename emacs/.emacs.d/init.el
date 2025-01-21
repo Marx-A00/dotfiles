af19381b1f87a4ee207cf52cbe67a5df98705afe
Marcos Andrade <mnandrade1999@gmail.com>
2025-01-21 10:56:55 -0600

    added doom-themes and doom-sourcerer

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -59,6 +59,13 @@
 ;; Still need to fix #file showing up maybe
 
 ;; Startup UI
+
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-sourcerer))
+
+
 (set-frame-parameter (selected-frame) 'alpha '(60 50))
 (setq inhibit-startup-message t)
 (setq display-line-numbers-type 'relative)
@@ -67,6 +74,7 @@
 
 ;; UX
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit) ; Make ESC quit prompts
+(setq visible-bell t)
 
 
 ;; org (kinda not really)
@@ -136,3 +144,16 @@
 
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("0d2c5679b6d087686dcfd4d7e57ed8e8aedcccc7f1a478cd69704c02e4ee36fe" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
