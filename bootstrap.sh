6d286457c0fb1fdd97c6669f1c1edc6637f94a78
Marx-A00 <mnandrade1999@gmail.com>
2025-12-03 12:32:38 -0600

    really not that many changes, but org-agend seems to be working lol

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,4 +6,11 @@ ln -sf "$HOME/.dotfiles/yabai/yabairc" "$HOME/.yabairc"
 ln -sf "$HOME/.dotfiles/skhd/skhdrc" "$HOME/.skhdrc"
 ln -sf "$HOME/.dotfiles/sketchybar" "$HOME/.config/sketchybar"
 
+# Emacs LaunchAgents
+ln -sf "$HOME/.dotfiles/emacs/com.marcosandrade.emacsdaemon.plist" "$HOME/Library/LaunchAgents/"
+ln -sf "$HOME/.dotfiles/emacs/com.marcosandrade.emacsclient.plist" "$HOME/Library/LaunchAgents/"
+
 echo "All symlinks created!"
+echo "To activate Emacs LaunchAgents, run:"
+echo "  launchctl load ~/Library/LaunchAgents/com.marcosandrade.emacsdaemon.plist"
+echo "  launchctl load ~/Library/LaunchAgents/com.marcosandrade.emacsclient.plist"
